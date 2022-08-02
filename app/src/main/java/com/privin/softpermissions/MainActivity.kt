be9f79c2f7ee11126955820caa610b2b5f0ab290
privindolleth <privin@okcredit.in>
2022-08-02 08:33:21 +0530

    Library created to handle permissions

--- app/src/main/java/com/privin/softpermissions/MainActivity.kt
+++ app/src/main/java/com/privin/softpermissions/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.privin.softpermissions
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnCamera = findViewById<Button>(R.id.btnReqCam)
+        val btnCameraAndStorage = findViewById<Button>(R.id.btnReqCamAndStorage)
+        btnCamera.setOnClickListener {
+            requestCameraPermission()
+        }
+        btnCameraAndStorage.setOnClickListener {
+            requestCameraAndStoragePermission()
+        }
+    }
+
+    private fun requestCameraPermission() {
+        SoftPermissions.requiredPermission(Manifest.permission.CAMERA)
+            .handlePermanentlyDeniedPermission()
+            .handle(this) { permissionStatus ->
+                Toast.makeText(this, permissionStatus.name, Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun requestCameraAndStoragePermission() {
+        SoftPermissions.requiredMultiplePermissions(
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+            .handlePermanentlyDeniedPermission()
+            .handle(this) { permissionMap, allPermissionsGranted ->
+                Toast.makeText(this, allPermissionsGranted.toString(), Toast.LENGTH_SHORT).show()
+            }
     }
 }
---
+++ softPermissions/src/main/java/com/privin/softpermissions/SoftPermissions.kt
@@ -0,0 +1,188 @@
+package com.privin.softpermissions
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
+
+class SoftPermissions : AppCompatActivity() {
+
+    private lateinit var rootView: ConstraintLayout
+    private lateinit var msgContainerView: View
+
+    private var shouldHandlePermanentlyDenied: Boolean = false
+
+    private lateinit var mode: String
+    private lateinit var permission: String
+    private lateinit var permissions: Array<String>
+
+    enum class PermissionStatus {
+        GRANTED, DENIED, PERMANENTLY_DENIED
+    }
+
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { permissionGranted ->
+            when {
+                permissionGranted -> {
+                    permissionResult.invoke(PermissionStatus.GRANTED)
+                    finish()
+                }
+                shouldShowRequestPermissionRationale(permission) -> {
+                    permissionResult.invoke(PermissionStatus.DENIED)
+                    finish()
+                }
+                else -> {
+                    if (shouldHandlePermanentlyDenied) {
+                        showNoPermissionMsg()
+                    }
+                    permissionResult.invoke(PermissionStatus.PERMANENTLY_DENIED)
+                }
+            }
+
+        }
+
+    private val multipleRequestPermissions =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissionMap ->
+            var isAnyPermissionPermanentlyDenied = false
+            val softPermissionMap = hashMapOf<String, PermissionStatus>()
+            permissionMap.entries.forEach {
+                val value = when {
+                    it.value -> {
+                        PermissionStatus.GRANTED
+                    }
+                    shouldShowRequestPermissionRationale(it.key) -> {
+                        PermissionStatus.DENIED
+                    }
+                    else -> {
+                        isAnyPermissionPermanentlyDenied = true
+                        PermissionStatus.PERMANENTLY_DENIED
+                    }
+                }
+                softPermissionMap[it.key] = value
+            }
+
+            multiplePermissionResult.invoke(
+                softPermissionMap,
+                isAnyPermissionPermanentlyDenied.not()
+            )
+
+            if (shouldHandlePermanentlyDenied && isAnyPermissionPermanentlyDenied) {
+                showNoPermissionMsg()
+            } else {
+                finish()
+            }
+
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_soft_permissions)
+        rootView = findViewById(R.id.root)
+        msgContainerView = findViewById(R.id.msg_container)
+        init()
+        initListeners()
+        makeRequest()
+    }
+
+    private fun init() {
+        mode = intent?.getStringExtra(ARG_MODE) ?: "single"
+        permission = intent?.getStringExtra(ARG_PERMISSION) ?: ""
+        permissions = intent?.getStringArrayExtra(ARG_MULTIPLE_PERMISSIONS) ?: arrayOf("")
+        shouldHandlePermanentlyDenied =
+            intent?.getBooleanExtra(ARG_HANDLE_PERMANENTLY_DENIED, false) ?: false
+    }
+
+    private fun makeRequest() {
+        if (mode == "single") {
+            requestPermission(permission)
+        } else {
+            requestPermissions(permissions)
+        }
+    }
+
+    private fun initListeners() {
+        rootView.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun requestPermission(permission: String) {
+        if (permission.isEmpty()) return
+        requestPermission.launch(permission)
+    }
+
+    private fun requestPermissions(permissions: Array<String>) {
+        if (permissions.isEmpty()) return
+        multipleRequestPermissions.launch(permissions)
+    }
+
+    private fun showNoPermissionMsg() {
+        val snackBar = Snackbar.make(
+            msgContainerView,
+            resources.getString(R.string.no_permission_msg),
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snackBar.setAction(resources.getString(R.string.settings)) {
+            val intent = Intent()
+            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            val uri = Uri.fromParts("package", packageName, null)
+            intent.data = uri
+            startActivity(intent)
+            finish()
+        }
+        snackBar.show()
+    }
+
+    companion object {
+        private lateinit var permissionResult: (PermissionStatus) -> Unit
+        private lateinit var multiplePermissionResult: (HashMap<String, PermissionStatus>, Boolean) -> Unit
+
+        private const val ARG_MODE = "mode"
+        private const val ARG_PERMISSION = "permission"
+        private const val ARG_MULTIPLE_PERMISSIONS = "multiple_permissions"
+        private const val ARG_HANDLE_PERMANENTLY_DENIED = "handle_permanently_denied"
+
+        private val permissionIntent = Intent()
+
+        fun requiredPermission(permission: String): Companion {
+            permissionIntent.putExtra(ARG_PERMISSION, permission)
+            return this
+        }
+
+        fun requiredMultiplePermissions(vararg permissions: String): Companion {
+            permissionIntent.putExtra(ARG_MULTIPLE_PERMISSIONS, permissions)
+            return this
+        }
+
+        // Shows snackBar message with action to settings page to enable permissions
+        fun handlePermanentlyDeniedPermission(): Companion {
+            permissionIntent.putExtra(ARG_HANDLE_PERMANENTLY_DENIED, true)
+            return this
+        }
+
+        //handles single request permission
+        fun handle(context: Context, permissionResult: (PermissionStatus) -> Unit) {
+            permissionIntent.putExtra(ARG_MODE, "single")
+            permissionIntent.setClass(context, SoftPermissions::class.java)
+            this.permissionResult = permissionResult
+            context.startActivity(permissionIntent)
+        }
+
+        //handles multiple request permissions
+        fun handle(
+            context: Context,
+            multiplePermissionResult: (HashMap<String, PermissionStatus>, Boolean) -> Unit
+        ) {
+            permissionIntent.putExtra(ARG_MODE, "multiple")
+            permissionIntent.setClass(context, SoftPermissions::class.java)
+            this.multiplePermissionResult = multiplePermissionResult
+            context.startActivity(permissionIntent)
+        }
+    }
+}
