9507ab15b4478ba3cafb8a04471088cf7ec88955
privindolleth <privin@okcredit.in>
2022-08-03 21:30:05 +0530

    Fixed code removed by mistake

--- app/src/main/java/com/privin/softpermissions_demo/MainActivity.kt
+++ app/src/main/java/com/privin/softpermissions_demo/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
         val btnCamera = findViewById<Button>(R.id.btnReqCam)
         val btnCameraAndStorage = findViewById<Button>(R.id.btnReqCamAndStorage)
         val permission = SoftPermissions.checkPermission(this, Manifest.permission.CAMERA)
-        Toast.makeText(this, "Camera permission: "+ permission.name, Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Camera permission: " + permission.name, Toast.LENGTH_SHORT).show()
         btnCamera.setOnClickListener {
             requestCameraPermission()
         }
@@ -24,18 +24,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun requestCameraPermission() {
-        SoftPermissions.handle(this) { permissionStatus ->
-            Toast.makeText(this, permissionStatus.name, Toast.LENGTH_SHORT).show()
-        }
+        SoftPermissions.requiredPermission(Manifest.permission.CAMERA)
+            .handlePermanentlyDeniedPermission()
+            .handle(this) { permissionStatus ->
+                Toast.makeText(this, permissionStatus.name, Toast.LENGTH_SHORT).show()
+            }
     }
 
     private fun requestCameraAndStoragePermission() {
-        SoftPermissions.handle(this) { permissionMap, allPermissionsGranted ->
-            if (allPermissionsGranted) {
-                Toast.makeText(this, "All permissions are granted", Toast.LENGTH_LONG).show()
-            } else {
-                Toast.makeText(this, "Some permissions are denied", Toast.LENGTH_LONG).show()
+        SoftPermissions.requiredMultiplePermissions(
+            Manifest.permission.CAMERA,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+            .handlePermanentlyDeniedPermission()
+            .handle(this) { permissionMap, allPermissionsGranted ->
+                if (allPermissionsGranted) {
+                    Toast.makeText(this, "All permissions are granted", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(this, "Some permissions are denied", Toast.LENGTH_LONG).show()
+                }
             }
-        }
     }
 }
