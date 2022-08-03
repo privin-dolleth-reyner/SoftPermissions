62c2934e0684c4f3b4c7f4ee65c6e8778c5e6fae
privindolleth <privin@okcredit.in>
2022-08-03 09:06:55 +0530

    - Support for android api 21 - CheckPermission function added - Not Requested PermissionStatus added

--- softPermissions/src/main/java/com/privin/softpermissions/SoftPermissions.kt
+++ softPermissions/src/main/java/com/privin/softpermissions/SoftPermissions.kt
@@ -1,14 +1,20 @@
 package com.privin.softpermissions
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Build
+import android.os.Build.VERSION_CODES.M
 import android.os.Bundle
 import android.provider.Settings
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 
 class SoftPermissions : AppCompatActivity() {
@@ -23,9 +29,17 @@ class SoftPermissions : AppCompatActivity() {
     private lateinit var permissions: Array<String>
 
     enum class PermissionStatus {
-        GRANTED,  // Permission granted
-        DENIED,   // Permission denied once, show custom msg why this permission is required for user to enable permissions and request again
-        PERMANENTLY_DENIED // Permission denied, will have to manually enable from settings
+        /** Permission granted */
+        GRANTED,
+
+        /** // Permission denied once, show custom msg why this permission is required for user to enable permissions and request again */
+        DENIED_ONCE,
+
+        /** Permission denied, will have to manually enable from settings */
+        DENIED,
+
+        /** Permission was not requested before */
+        NOT_REQUESTED
     }
 
     private val requestPermission =
@@ -35,15 +49,15 @@ class SoftPermissions : AppCompatActivity() {
                     permissionResult.invoke(PermissionStatus.GRANTED)
                     finish()
                 }
-                shouldShowRequestPermissionRationale(permission) -> {
-                    permissionResult.invoke(PermissionStatus.DENIED)
+                Build.VERSION.SDK_INT >= M && shouldShowRequestPermissionRationale(permission) -> {
+                    permissionResult.invoke(PermissionStatus.DENIED_ONCE)
                     finish()
                 }
                 else -> {
                     if (shouldHandlePermanentlyDenied) {
                         showNoPermissionMsg()
                     }
-                    permissionResult.invoke(PermissionStatus.PERMANENTLY_DENIED)
+                    permissionResult.invoke(PermissionStatus.DENIED)
                 }
             }
 
@@ -58,12 +72,12 @@ class SoftPermissions : AppCompatActivity() {
                     it.value -> {
                         PermissionStatus.GRANTED
                     }
-                    shouldShowRequestPermissionRationale(it.key) -> {
-                        PermissionStatus.DENIED
+                    Build.VERSION.SDK_INT >= M && shouldShowRequestPermissionRationale(it.key) -> {
+                        PermissionStatus.DENIED_ONCE
                     }
                     else -> {
                         isAnyPermissionPermanentlyDenied = true
-                        PermissionStatus.PERMANENTLY_DENIED
+                        PermissionStatus.DENIED
                     }
                 }
                 softPermissionMap[it.key] = value
@@ -116,12 +130,21 @@ class SoftPermissions : AppCompatActivity() {
 
     private fun requestPermission(permission: String) {
         if (permission.isEmpty()) return
+        logRequest(this, permission)
         requestPermission.launch(permission)
     }
 
     private fun requestPermissions(permissions: Array<String>) {
         if (permissions.isEmpty()) return
+        permissions.forEach { permission ->
+            logRequest(this, permission)
+        }
         multipleRequestPermissions.launch(permissions)
+    }
+
+    private fun logRequest(context: Context, permission: String) {
+        val sharedPref = context.getSharedPreferences(SOFT_PERMISSIONS_PREF, Context.MODE_PRIVATE)
+        sharedPref.edit().putBoolean(permission, true).apply()
     }
 
     private fun showNoPermissionMsg() {
@@ -145,12 +168,51 @@ class SoftPermissions : AppCompatActivity() {
         private lateinit var permissionResult: (PermissionStatus) -> Unit
         private lateinit var multiplePermissionResult: (HashMap<String, PermissionStatus>, Boolean) -> Unit
 
+        private const val SOFT_PERMISSIONS_PREF = "soft_permissions_pref"
+
         private const val ARG_MODE = "mode"
         private const val ARG_PERMISSION = "permission"
         private const val ARG_MULTIPLE_PERMISSIONS = "multiple_permissions"
         private const val ARG_HANDLE_PERMANENTLY_DENIED = "handle_permanently_denied"
 
         private val permissionIntent = Intent()
+
+        private fun isNeverRequested(context: Context, permission: String): Boolean {
+            val sharedPref =
+                context.getSharedPreferences(SOFT_PERMISSIONS_PREF, Context.MODE_PRIVATE)
+            return sharedPref.getBoolean(permission, false).not()
+        }
+
+        fun checkPermission(activity: Activity, permission: String): PermissionStatus {
+            val shouldShowRationale = activity.run {
+                Build.VERSION.SDK_INT >= M && shouldShowRequestPermissionRationale(permission)
+            }
+
+            return getPermissionStatus(activity, permission, shouldShowRationale)
+        }
+
+        fun checkPermission(fragment: Fragment, permission: String): PermissionStatus {
+            val shouldShowRationale = fragment.run {
+                Build.VERSION.SDK_INT >= M && shouldShowRequestPermissionRationale(permission)
+            }
+
+            return getPermissionStatus(fragment.requireContext(), permission, shouldShowRationale)
+        }
+
+        private fun getPermissionStatus(
+            context: Context,
+            permission: String,
+            shouldShowRationale: Boolean
+        ): PermissionStatus {
+            return when (ContextCompat.checkSelfPermission(context, permission)) {
+                PackageManager.PERMISSION_GRANTED -> PermissionStatus.GRANTED
+                else -> {
+                    if (shouldShowRationale) PermissionStatus.DENIED_ONCE
+                    else if (isNeverRequested(context, permission)) PermissionStatus.NOT_REQUESTED
+                    else PermissionStatus.DENIED
+                }
+            }
+        }
 
         fun requiredPermission(permission: String): Companion {
             permissionIntent.putExtra(ARG_PERMISSION, permission)
@@ -168,7 +230,7 @@ class SoftPermissions : AppCompatActivity() {
             return this
         }
 
-        //handles single request permission
+        // handles single request permission
         fun handle(context: Context, permissionResult: (PermissionStatus) -> Unit) {
             permissionIntent.putExtra(ARG_MODE, "single")
             permissionIntent.setClass(context, SoftPermissions::class.java)
@@ -176,9 +238,9 @@ class SoftPermissions : AppCompatActivity() {
             context.startActivity(permissionIntent)
         }
 
-        //handles multiple request permissions
-        // returns hashmap of permission and their permission Status {PermissionStatus}
-        // returns true when all the permission in the list were granted.
+        /* handles multiple request permissions
+        returns hashmap of permission and their permission Status {PermissionStatus}
+        returns true when all the permission in the list were granted.*/
         fun handle(
             context: Context,
             multiplePermissionResult: (HashMap<String, PermissionStatus>, Boolean) -> Unit
