be62e318731096778d9f8c9cbead2162bbb426c6
privindolleth <privin@okcredit.in>
2022-08-03 19:53:21 +0530

    Fixed duplicate package name

--- app/src/main/java/com/privin/softpermissions_demo/MainActivity.kt
+++ app/src/main/java/com/privin/softpermissions_demo/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.privin.softpermissions
+package com.privin.softpermissions_demo
 
 import android.Manifest
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.privin.softpermissions.SoftPermissions
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,25 +24,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun requestCameraPermission() {
-        SoftPermissions.requiredPermission(Manifest.permission.CAMERA)
-            .handlePermanentlyDeniedPermission()
-            .handle(this) { permissionStatus ->
-                Toast.makeText(this, permissionStatus.name, Toast.LENGTH_SHORT).show()
-            }
+        SoftPermissions.handle(this) { permissionStatus ->
+            Toast.makeText(this, permissionStatus.name, Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun requestCameraAndStoragePermission() {
-        SoftPermissions.requiredMultiplePermissions(
-            Manifest.permission.CAMERA,
-            Manifest.permission.WRITE_EXTERNAL_STORAGE
-        )
-            .handlePermanentlyDeniedPermission()
-            .handle(this) { permissionMap, allPermissionsGranted ->
-                if (allPermissionsGranted) {
-                    Toast.makeText(this, "All permissions are granted", Toast.LENGTH_LONG).show()
-                } else {
-                    Toast.makeText(this, "Some permissions are denied", Toast.LENGTH_LONG).show()
-                }
+        SoftPermissions.handle(this) { permissionMap, allPermissionsGranted ->
+            if (allPermissionsGranted) {
+                Toast.makeText(this, "All permissions are granted", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Some permissions are denied", Toast.LENGTH_LONG).show()
             }
+        }
     }
 }
